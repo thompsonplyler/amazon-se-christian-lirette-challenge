077f03c35cb6072221d392ef669f5fd63e85cf73
thompsonplyler <nealok.g@gmail.com>
2022-06-09 14:18:11 -0400

    testing code

--- cl.js
+++ cl.js
@@ -1,7 +1,10 @@
+console.log("Reached the code... sending fetch...")
 fetch("https://dev--code-challenge-receipt.thompson-plyler.autocode.gg/", {
     method: "post",
     headers: {
       "Content-Type": 'application/json',
       "Accepts": 'application/json'
       },
-    body: JSON.stringify({title:"Test for Assignment", subject: "Christian Lirette Assignment Received"})})
+    body: JSON.stringify({title:"Test for Assignment", subject: "Christian Lirette Assignment Received"})}).then(r=>r.json()).then(response=>{
+        console.log("Received after post: ", response)
+    })
